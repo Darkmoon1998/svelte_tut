6865db8993f3a26621c2b3067ef978cd84a819bd
DarkMoon1998 <gautamsganesh@gmail.com>
2022-04-04 15:28:43 -0300

    commit

--- src/routes/test.svelte
+++ src/routes/test.svelte
@@ -1,50 +1,49 @@
-<script context="module">
-	import { browser, dev } from '$app/env';
+<script>
+	import * as d3 from 'd3';
+	let name = '';
+	import { browser } from '$app/env';
+	const url =
+		'https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv';
 
-	// we don't need any JS on this page, though we'll load
-	// it in dev so that we get hot module replacement...
-	export const hydrate = dev;
+	let data = [];
+	fetch(url).then(async (response) => {
+		let body = await response.text();
+		body = body.split('\n');
+		for (let i = 1; i < body.length; i++) {
+			let row = body[i].split(',');
+			data.push([parseFloat(row[0]), parseFloat(row[1])]);
+		}
+		data = data;
+		let max_data = 0;
+		data.forEach((dd) => dd.forEach((d) => (max_data = Math.max(max_data, Math.abs(d)))));
 
-	// ...but if the client-side router is already loaded
-	// (i.e. we came here from elsewhere in the app), use it
-	export const router = browser;
+		let x = d3.scaleLinear().domain([-max_data, max_data]).range([0, size.width]);
+		let y = d3.scaleLinear().domain([max_data, -max_data]).range([0, size.width]);
+		console.log(max_data, data, x(1, 1));
 
-	// since there's no dynamic data here, we can prerender
-	// it so that it gets served as a static asset in prod
-	export const prerender = true;
+		let dataplotly = {};
+		dataplotly.x = data.map((d) => d[0]);
+		dataplotly.y = data.map((d) => d[1]);
+		dataplotly.mode = 'markers';
+		dataplotly.type = 'scatter';
+		if (Plotly) new Plotly.newPlot(plot, [dataplotly]);
+	});
+	let size = { width: 300, height: 300 };
+	let x = () => {};
+	let y = () => {};
+	let plot;
 </script>
 
 <svelte:head>
-	<title>About</title>
+	<script src="https://cdn.plot.ly/plotly-latest.min.js" type="text/javascript"></script>
 </svelte:head>
 
-<div class="content">
-	<h1>About this app</h1>
-
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
-
-	<!-- TODO lose the @next! -->
-	<pre>npm init svelte@next</pre>
-
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href="/todos">TODOs</a> page illustrates SvelteKit's data loading and form handling. Try using
-		it with JavaScript disabled!
-	</p>
+<div>
+	<svg width={size.width} height={size.height}>
+		{#each data as xy}
+			<circle cx={x(xy[0])} cy={y(xy[1])} r="5" fill="red" />
+		{/each}
+	</svg>
 </div>
 
-<style>
-	.content {
-		width: 100%;
-		max-width: var(--column-width);
-		margin: var(--column-margin-top) auto 0 auto;
-	}
-</style>
+<div bind:this={plot} />
